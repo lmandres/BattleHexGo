5d709071cfc6dbefe2e173869cefb5e32af3302e
landres-cogentpeople <landres@cogentpeople.com>
2023-03-03 09:23:59 -0500

    Added session test.

--- battlehex.go
+++ battlehex.go
@@ -1,42 +1,29 @@
 package main
 
 import (
-    "crypto/rand"
-    "encoding/base64"
     //"encoding/json"
-    //"fmt"
-    "io"
+    "fmt"
     //"io/ioutil"
     "log"
     "net/http"
     "os"
-    "time"
 
     "battle-hex-go/frontend"
 
+    "github.com/gorilla/sessions"
     //"github.com/coreos/go-oidc/v3/oidc"
     //"golang.org/x/net/context"
     //"golang.org/x/oauth2"
 )
 
-type Secret struct {
-    Web Web `json:"web"`
-}
-
-type Web struct {
-    ClientID string `json:"client_id"`
-    ProjectID string `json:"project_id"`
-    AuthURI string `json:"auth_uri"`
-    TokenURI string `json:"token_uri"`
-    AuthProviderX509CertURL string `json:"auth_provider_x509_cert_url"`
-    ClientSecret string `json:"client_secret"`
-    RedirectURIs []string `json:"redirect_uris"`
-}
+var store = sessions.NewCookieStore([]byte("development_session_key"))
 
 func main() {
 
     fs := http.FileServer(http.Dir("./static"))
     http.HandleFunc("/battlehex_vs_js_ai_v1.1", frontend.BattleHexJSHandler)
+    http.HandleFunc("/set_session", setSessionHandler)
+    http.HandleFunc("/get_session", getSessionHandler)
     http.Handle("/static/", http.StripPrefix("/static/", fs))
 
     port :=  os.Getenv("PORT")
@@ -51,21 +38,37 @@ func main() {
     }
 }
 
-func randString(nByte int) (string, error) {
-    b := make([]byte, nByte)
-    if _, err := io.ReadFull(rand.Reader, b); err != nil {
-        return "", err
+func setSessionHandler(w http.ResponseWriter, r *http.Request) {
+    // Get a session. Get() always returns a session, even if empty.
+    session, err := store.Get(r, "session-name")
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
     }
-    return base64.RawURLEncoding.EncodeToString(b), nil
+
+    // Set some session values.
+    session.Values["foo"] = "bar"
+    session.Values[42] = 43
+    // Save it before we write to the response/return from the handler.
+    err = session.Save(r, w)
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+    }
+
+    fmt.Fprintf(w, "session.Values[\"foo\"] = %s<br />\n", session.Values["foo"])
+    fmt.Fprintf(w, "session.Values[42] = %d<br />\n", session.Values[42])
 }
 
-func setCallbackCookie(w http.ResponseWriter, r *http.Request, name, value string) {
-    c := &http.Cookie{
-        Name:     name,
-        Value:    value,
-        MaxAge:   int(time.Hour.Seconds()),
-        Secure:   r.TLS != nil,
-        HttpOnly: true,
+func getSessionHandler(w http.ResponseWriter, r *http.Request) {
+
+    session, err := store.Get(r, "session-name")
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
     }
-    http.SetCookie(w, c)
+
+
+    fmt.Fprintf(w, "session.Values[\"foo\"] = %s<br />\n", session.Values["foo"])
+    fmt.Fprintf(w, "session.Values[42] = %d<br />\n", session.Values[42])
 }
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.16
 
 require (
 	github.com/coreos/go-oidc/v3 v3.2.0 // indirect
+	github.com/gorilla/sessions v1.2.1 // indirect
 	golang.org/x/net v0.0.0-20220607020251-c690dde0001d // indirect
 	golang.org/x/oauth2 v0.0.0-20220608161450-d0670ef3b1eb // indirect
 )
