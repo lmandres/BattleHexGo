1a1ba6d79bae7a6be1f4e67761b1f3b8ad0045fe
lmandres <lmandres@yahoo.com>
2023-03-05 20:41:06 -0500

    Make changes to add authentication with Google.

--- battlehex.go
+++ battlehex.go
@@ -2,21 +2,45 @@ package main
 
 import (
     "context"
+    "encoding/json"
     "fmt"
     "log"
     "net/http"
     "os"
+    "time"
 
+    "cloud.google.com/go/compute/metadata"
     "cloud.google.com/go/firestore"
+    "github.com/golang-jwt/jwt"
     firestoregorilla "github.com/GoogleCloudPlatform/firestore-gorilla-sessions"
 
     "battle-hex-go/frontend"
 )
 
-var store *firestoregorilla.Store
+const sessionName string = "session-name"
 
 func main() {
 
+    projectID, err := metadata.ProjectID()
+    if err != nil {
+        log.Fatalf("metadata.ProjectID: %v", err)
+    }
+
+    session, err := newSession(projectID)
+    if err != nil {
+        log.Fatalf("newSessionStore: %v", err)
+    }
+
+    setSessionHandler := getSetSessionHandlerFunc(session)
+    getSessionHandler := getGetSessionHandlerFunc(session)
+
+    certs, aud, err := getAuthItems()
+    if err != nil {
+        log.Fatalf("getAuthItems: %v", err)
+    }
+    authIndexHandler := getAuthIndexHandlerFunc(certs, aud)
+
+    http.HandleFunc("/", authIndexHandler)
     http.HandleFunc("/battlehex_vs_js_ai_v1.1", frontend.BattleHexJSHandler)
     http.HandleFunc("/set_session", setSessionHandler)
     http.HandleFunc("/get_session", getSessionHandler)
@@ -27,24 +51,13 @@ func main() {
         log.Printf("Defaulting to port %s", port)
     }
 
-    projectID := os.Getenv("GOOGLE_CLOUD_PROJECT")
-    if projectID == "" {
-        log.Fatal("GOOGLE_CLOUD_PROJECT must be set")
-    }
-
-    var err error
-    store, err = newSessionStore(projectID)
-    if err != nil {
-        log.Fatalf("newSessionStore: %v", err)
-    }
-
     log.Printf("Listening on port %s", port)
     if err := http.ListenAndServe(":"+port, nil); err != nil {
         log.Fatal(err)
     }
 }
 
-func newSessionStore(projectID string) (*firestoregorilla.Store, error) {
+func newSession(projectID string) (*firestoregorilla.Store, error) {
 
     ctx := context.Background()
     client, err := firestore.NewClient(ctx, projectID)
@@ -57,37 +70,151 @@ func newSessionStore(projectID string) (*firestoregorilla.Store, error) {
         log.Fatalf("firestoregorilla.New: %v", err)
     }
 
+
     return store, nil
 }
 
-func setSessionHandler(w http.ResponseWriter, r *http.Request) {
-    // Get a session. Get() always returns a session, even if empty.
-    session, err := store.Get(r, "session-name")
+func getAuthItems() (map[string]string, string, error) {
+
+    certs, err := certificates()
     if err != nil {
-        http.Error(w, err.Error(), http.StatusInternalServerError)
-        return
+        return nil, "", err
     }
 
-    // Set some session values.
-    session.Values["foo"] = "bar"
-    // Save it before we write to the response/return from the handler.
-    err = session.Save(r, w)
+    aud, err := audience()
     if err != nil {
-        http.Error(w, err.Error(), http.StatusInternalServerError)
-        return
+        return certs, "", err
     }
 
-    fmt.Fprintf(w, "session.Values[\"foo\"] = %s<br />\n", session.Values["foo"])
+    return certs, aud, nil
 }
 
-func getSessionHandler(w http.ResponseWriter, r *http.Request) {
+func getAuthIndexHandlerFunc(certs map[string]string, aud string) func(http.ResponseWriter, *http.Request) {
 
-    session, err := store.Get(r, "session-name")
+    // function responds to requests with our greeting.
+    return func (w http.ResponseWriter, r *http.Request) {
+        if r.URL.Path != "/" {
+            http.NotFound(w, r)
+            return
+        }
+
+        assertion := r.Header.Get("X-Goog-IAP-JWT-Assertion")
+        if assertion == "" {
+           fmt.Fprintln(w, "No Cloud IAP header found.")
+           return
+        }
+        email, userID, err := validateAssertion(assertion, certs, aud)
+        if err != nil {
+            log.Println(err)
+            fmt.Fprintln(w, "Could not validate assertion. Check app logs.")
+            return
+        }
+
+        fmt.Fprintf(w, "Hello %s with userID = %s\n", email, userID)
+    }
+}
+
+func getSetSessionHandlerFunc(store *firestoregorilla.Store) func(http.ResponseWriter, *http.Request) {
+
+    return func (w http.ResponseWriter, r *http.Request) {
+
+        // Get a session. Get() always returns a session, even if empty.
+        session, err := store.Get(r, sessionName)
+        if err != nil {
+            http.Error(w, err.Error(), http.StatusInternalServerError)
+        }
+
+        // Set some session values.
+        session.Values["foo"] = "bar"
+        fooString := r.URL.Query().Get("foo")
+        if fooString != "" {
+            session.Values["foo"] = fooString
+        }
+
+        // Save it before we write to the response/return from the handler.
+        err = session.Save(r, w)
+        if err != nil {
+            http.Error(w, err.Error(), http.StatusInternalServerError)
+            return
+        }
+
+        fmt.Fprintf(w, "session.Values[\"foo\"] = %s<br />\n", session.Values["foo"])
+    }
+}
+
+func getGetSessionHandlerFunc(store *firestoregorilla.Store) func(http.ResponseWriter, *http.Request) {
+
+    return func (w http.ResponseWriter, r *http.Request) {
+        // Get a session. Get() always returns a session, even if empty.
+        session, err := store.Get(r, sessionName)
+        if err != nil {
+            http.Error(w, err.Error(), http.StatusInternalServerError)
+        }
+        fmt.Fprintf(w, "session.Values[\"foo\"] = %s<br />\n", session.Values["foo"])
+    }
+}
+
+// validateAssertion validates assertion was signed by Google and returns the
+// associated email and userID.
+func validateAssertion(assertion string, certs map[string]string, aud string) (email string, userID string, err error) {
+    token, err := jwt.Parse(assertion, func(token *jwt.Token) (interface{}, error) {
+        keyID := token.Header["kid"].(string)
+
+        _, ok := token.Method.(*jwt.SigningMethodECDSA)
+        if !ok {
+            return nil, fmt.Errorf("unexpected signing method: %q", token.Header["alg"])
+        }
+
+        cert := certs[keyID]
+        return jwt.ParseECPublicKeyFromPEM([]byte(cert))
+    })
+
     if err != nil {
-        http.Error(w, err.Error(), http.StatusInternalServerError)
-        return
+        return "", "", err
     }
 
+    claims, ok := token.Claims.(jwt.MapClaims)
+    if !ok {
+        return "", "", fmt.Errorf("could not extract claims (%T): %+v", token.Claims, token.Claims)
+    }
 
-    fmt.Fprintf(w, "session.Values[\"foo\"] = %s<br />\n", session.Values["foo"])
+    if claims["aud"].(string) != aud {
+        return "", "", fmt.Errorf("mismatched audience. aud field %q does not match %q", claims["aud"], aud)
+    }
+    return claims["email"].(string), claims["sub"].(string), nil
+}
+
+// audience returns the expected audience value for this service.
+func audience() (string, error) {
+    projectNumber, err := metadata.NumericProjectID()
+    if err != nil {
+        return "", fmt.Errorf("metadata.NumericProjectID: %v", err)
+    }
+
+    projectID, err := metadata.ProjectID()
+    if err != nil {
+        return "", fmt.Errorf("metadata.ProjectID: %v", err)
+    }
+
+    return "/projects/" + projectNumber + "/apps/" + projectID, nil
+}
+
+// certificates returns Cloud IAP's cryptographic public keys.
+func certificates() (map[string]string, error) {
+    const url = "https://www.gstatic.com/iap/verify/public_key"
+    client := http.Client{
+        Timeout: 5 * time.Second,
+    }
+    resp, err := client.Get(url)
+    if err != nil {
+        return nil, fmt.Errorf("Get: %v", err)
+    }
+
+    var certs map[string]string
+    dec := json.NewDecoder(resp.Body)
+    if err := dec.Decode(&certs); err != nil {
+        return nil, fmt.Errorf("Decode: %v", err)
+    }
+
+    return certs, nil
 }
--- go.mod
+++ go.mod
@@ -3,8 +3,10 @@ module battle-hex-go
 go 1.16
 
 require (
+	cloud.google.com/go/compute/metadata v0.2.1 // indirect
 	cloud.google.com/go/firestore v1.9.0
 	github.com/GoogleCloudPlatform/firestore-gorilla-sessions v0.1.0
 	github.com/coreos/go-oidc/v3 v3.2.0 // indirect
+	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
 	github.com/gorilla/sessions v1.2.1
 )
